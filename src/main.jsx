6f734873555df3e2bc813d6b9b6be6c7b30bba56
unknown <jyj2702@gmail.com>
2026-04-28 13:48:00 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GolfScoringApp from './GolfScoringApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GolfScoringApp />
+  </React.StrictMode>
+)
